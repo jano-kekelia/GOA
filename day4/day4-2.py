74cbd0374e7f794b68f4b02a3b7ff9480e45e18c
jano-kekelia <kjansughi@gmail.com>
2024-08-16 12:57:47 +0400

    commit

--- day4/day4-2.py
+++
@@ -1,8 +0,0 @@
-number1 = int(input("enter your number: "))
-
-number = int(input("enter your number1: "))
-
-print(str(number) + "+" + str(number1) + "=" + str(number+number1))
-
-
-
--- day6/day6.py
+++
@@ -1,5 +0,0 @@
-number=10
-number1=5.5
-
-number3=(number  + number1)
-number4=(number - number1)
